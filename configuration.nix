8e404fb1cc3a82cb811be0caee30ae745997aee8
Jaime Nazar Anchorena <nam.jaime@gmail.com>
2026-03-22 12:07:34 -0300

    fix: Fix bluetooth

--- configuration.nix
+++ configuration.nix
@@ -103,7 +103,6 @@ in
 		vesktop
 		grim
 		qemu
-		samba
 		quickemu
 		cura-appimage
 		vscodium
@@ -142,6 +141,29 @@ in
 	enable = true;
 	platformTheme = "gnome";
 	style = "adwaita-dark";
+  };
+
+  # Bluetooth config
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
   };
 
   # Configure network proxy if necessary
